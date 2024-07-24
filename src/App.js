4ce237558b9ce3f5554161de59fc81eff6e4c038
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2024-07-24 12:05:42 +0100

    second version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 function App() {
   return (
     <div className="App">
-      <h1>Rehearsing my redux toolkit knowledge</h1>
+      <AddTodo />
+      <Todos />
     </div>
   );
 }
