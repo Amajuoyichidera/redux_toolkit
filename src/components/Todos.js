84f3589711c448531fc07db985bf261255a48f7f
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2024-07-30 12:37:32 +0100

    second version

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,69 +1,90 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { removeTodo, updateTodo } from '../features/todo/todoSlice';
-import { fetchData } from '../features/todo/todoSlice';
+import { removeTodo, updateTodo, fetchData } from '../features/todo/todoSlice';
 
 const Todos = () => {
-  const todos = useSelector((state) => state.todos);
+  const todos = useSelector((state) => state.todo.todos);
   const dispatch = useDispatch();
   const [input, setInput] = useState('');
   const [modal, setModal] = useState(false);
-  const [selectedTodo, setSelectedTodo] = useState('');
-  
+  const [selectedTodo, setSelectedTodo] = useState(null);
+
   const handleUpdate = (todo) => {
     setInput(todo.text);
     setSelectedTodo(todo);
     setModal(true);
-  }
+  };
 
   const handleSave = () => {
-    if(selectedTodo) {
-        dispatch(updateTodo({
-            id: selectedTodo.id,
-            text: input,
-        }))
-        setModal(false)
+    if (selectedTodo) {
+      dispatch(updateTodo({
+        id: selectedTodo.id,
+        text: input,
+      }));
+      setModal(false);
     }
-  }
+  };
 
-  const status = useSelector((state) => state.todos.status)
-  const items = useSelector((state) => state.todos.items)
-  const error = useSelector((state) => state.todos.error)
+  const status = useSelector((state) => state.todo.status);
+  const items = useSelector((state) => state.todo.items);
+  const error = useSelector((state) => state.todo.error);
 
   useEffect(() => {
     if(status === 'idle') {
-        dispatch(fetchData())
+        dispatch(fetchData());
     }
-  }, [status, dispatch])
+  },[status, dispatch])
+
+  const Data = () => {
+    switch (status) {
+        case 'loading':
+            return <div>Loading...</div>
+        case 'error':
+            return <div>Error: {error}</div>
+        case 'succeeded':
+            return (
+                <div>
+                    {items.map((item) => (
+                        <div key={item.id}>{item.title}</div>
+                    ))}
+                </div>
+            );
+        default:
+            return null;
+    }
+  }
 
   return (
     <div>
       {todos.map((todo) => (
         <div key={todo.id}>
-            <h1>{todo.text}</h1>
-            <button onClick={() => dispatch(removeTodo(todo.id))}>delete</button>
-            <button onClick={() => handleUpdate(todo)}>Edit</button>
+          <h1>{todo.text}</h1>
+          <button onClick={() => dispatch(removeTodo(todo.id))}>Delete</button>
+          <button onClick={() => handleUpdate(todo)}>Edit</button>
         </div>
       ))}
 
-    {modal && <div>
-        <input value={input} onChange={(e) => setInput(e.target.value)} />
-        <button onClick={handleSave}>save</button>
-        <button onClick={() => setModal(false)}>close</button>
-    </div> }
-
-    <h1>Fetched Items</h1>
-      {status === 'loading' && <p>Loading...</p>}
-      {status === 'error' && <p>Error: {error}</p>}
-      {status === 'succeeded' && (
-        <ul>
-          {items.map((item) => (
-            <li key={item.id}>{item.title}</li>
-          ))}
-        </ul>
+      {modal && (
+        <div>
+          <input value={input} onChange={(e) => setInput(e.target.value)} />
+          <button onClick={handleSave}>Save</button>
+          <button onClick={() => setModal(false)}>Close</button>
+        </div>
       )}
-    </div>
-  )
-}
 
-export default Todos
+      <h1>Fetched Items</h1>
+
+      {/* {status === 'loading' && <h1>Loading...</h1>}
+      {status === 'error' && <h1>Error: {error}</h1>}
+      {status === 'succeeded' && (<div>
+        {items.map((item) => (
+            <div key={item.id}>{item.title}</div>
+        ))}
+      </div>)} */}
+      {Data()}
+      
+    </div>
+  );
+};
+
+export default Todos;
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -2,54 +2,49 @@ import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    todos: [],
-    items: [],
-    status: 'idle',
-    error: null,    
-}
+  todos: [],
+  items: [],
+  status: 'idle',
+  error: null,
+};
 
 export const fetchData = createAsyncThunk('data/fetchData', async () => {
     const response = await axios.get('https://dummyjson.com/posts');
     return response.data.posts;
 })
-
-
 export const todoSlice = createSlice({
-    name: 'todo',
-    initialState,
-    reducers: {
-        addTodo: (state, action) => {
-            const todo = {
-                id: nanoid(),
-                text: action.payload,
-            }
-            state.todos.push(todo)
-        },
-        removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
-        },
-        updateTodo: (state, action) => {
-            const { id, text } = action.payload;
-            const newTodo = state.todos.find((todo) => todo.id === id)
-            if(newTodo) {
-                newTodo.text = text
-            }
-        }
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
     },
-    extraReducers: (builder) => {
-        builder.addCase(fetchData.pending, (state) => {
-            state.status = 'loading';
-        }).addCase(fetchData.fulfilled, (state, action) => {
-            state.status = 'succeeded';
-            state.items = action.payload;
-            console.log('Data fetched successfully:', action.payload);
-        }).addCase(fetchData.rejected, (state, action) => {
-            state.status = 'error';
-            state.error = action.error.message;
-        })
-    }
-})
-
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const newTodo = state.todos.find((todo) => todo.id === id);
+      if (newTodo) {
+        newTodo.text = text;
+      }
+    },
+  }, extraReducers: (builders) => {
+    builders.addCase(fetchData.pending, (state) => {
+        state.status = 'loading';
+    }).addCase(fetchData.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.items = action.payload;
+    }).addCase(fetchData.rejected, (state, action) => {
+        state.status = 'error';
+        state.error = action.error.message;
+    })
+  }
+});
 
 export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 export default todoSlice.reducer;
